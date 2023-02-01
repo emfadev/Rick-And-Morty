33eb4538faba8e9cf35a9ed713267d04b7da0e09
Ellery Fuenmayor <emfadev@gmail.com>
2023-01-31 21:01:35 -0500

    ignorar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { CardUser } from './components/Card';
 import { Card } from './components/Card';
 import { Layout } from './components/Layout';
 
