cedaf5d8d858bdc3805ba552e5b11f614971fc07
Mp-keyner <keyneroswaldo0@gmail.com>
2023-02-03 13:36:00 -0500

    Search Bar Implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Search } from './components/Search';
 import { CharacterList } from './components/CharacterList';
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Search/>
     <CharacterList/>
   </React.StrictMode>
 );
