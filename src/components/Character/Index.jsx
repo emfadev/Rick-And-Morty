726ea2994068ee77a43cd77528c9731d4ae50dca
Ellery Fuenmayor <emfadev@gmail.com>
2023-02-02 21:09:39 -0500

    update: cards style

--- src/components/Character/Index.jsx
+++ src/components/Character/Index.jsx
@@ -8,11 +8,11 @@ export const Character = ({characters}) => {
       <div className="characters">
         <div className="character">
           <img className="character__img" src={characters.image} alt="*" />
-          <p className="character__species">Specie: {characters.species}</p>
+          <p className="character__species">{characters.species}</p>
         </div>
         <div className="characters__container">
           <h2 className="characters__name">{characters.name}</h2>
-          <p className="characters__info">Status: <span>{characters.status}</span></p>
+          <p className={characters.status === "Alive" ? "characters__status" : "characters__status--dead" }><span>{characters.status}</span></p>
           <p className="characters__info">Location: <span>{characters.location.name}</span></p>
           <p className="characters__info">Origin: <span>{characters.origin.name}</span></p>
         </div>
